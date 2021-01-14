283aedcbed18f621b09ca84946fa3f405e8946be
Azizbek <arasulov0066@gmail.com>
2021-01-14 15:13:55 +0500

    fdsv

---
+++ script.js
@@ -0,0 +1,17 @@
+const NumOfFilms= +prompt("Сколько фильмов вы уже посмотрели","");
+const personalMovieDB={
+    count: NumOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+const a=prompt("Один из последних просмотренных фильмов?",""),
+      b=+prompt("На сколько оцените его?",""),
+       c=prompt("Один из последних просмотренных фильмов?",""),
+          d=+prompt("На сколько оцените его?","");
+          
+personalMovieDB.movie[a]=b;
+personalMovieDB.movie[c]=d;
+console.log(personalMovieDB);
+ 
