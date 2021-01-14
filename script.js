0126381130eab03996054ff80a6395bf044a152c
Azizbek <arasulov0066@gmail.com>
2021-01-14 18:12:49 +0500

    movieapp

--- script.js
+++ script.js
@@ -6,12 +6,29 @@ const personalMovieDB={
     genres: [],
     private: false
 };
-const a=prompt("Один из последних просмотренных фильмов?",""),
-      b=+prompt("На сколько оцените его?",""),
-       c=prompt("Один из последних просмотренных фильмов?",""),
-          d=+prompt("На сколько оцените его?","");
-          
-personalMovieDB.movie[a]=b;
-personalMovieDB.movie[c]=d;
+
+for (let i=0; i<2;i++){
+    
+    const a=prompt("Один из последних просмотренных фильмов?",""),
+    b=+prompt("На сколько оцените его?","");
+    
+    if (a!=null && b!=null && a!="" && b!="" && a.length<50 ) {
+         personalMovieDB.movie[a]=b; 
+    } else{
+        console.log("Error");
+        i--;
+}    
+}
+
+if (personalMovieDB.count<10) {
+    console.log("Просмотрено довольно мало фильмов");
+}
+else if(10<=personalMovieDB.count && personalMovieDB.coun<30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count>30) {
+    console.log("Вы киноман!!!");
+} else {
+    console.log("Ошибка");
+}
 console.log(personalMovieDB);
  
